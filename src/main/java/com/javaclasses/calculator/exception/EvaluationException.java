24662914a482ba533bd7e169356d936c3441bf99
kompike <kompanietskostya@gmail.com>
2016-06-23 03:08:20 +0300

    Created custom exception for handling errors in evaluated expression

---
+++ src/main/java/com/javaclasses/calculator/exception/EvaluationException.java
@@ -0,0 +1,26 @@
+package com.javaclasses.calculator.exception;
+
+/**
+ * Custom exception for handling wrong symbol's
+ * position in evaluated expression
+ */
+
+public class EvaluationException extends Exception {
+
+    private final int position;
+
+    public EvaluationException(String message, int position) {
+        super(message);
+        this.position = position;
+    }
+
+    @Override
+    public String getMessage() {
+        return super.getMessage() + position;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+}
