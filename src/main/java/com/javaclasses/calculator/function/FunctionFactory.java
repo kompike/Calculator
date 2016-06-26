c026c9e9fe112a57f0ed8524bdfbff49cc818a39
kompike <kompanietskostya@gmail.com>
2016-06-26 21:58:50 +0300

    Finished sum function realisation

--- src/main/java/com/javaclasses/calculator/function/FunctionFactory.java
+++ src/main/java/com/javaclasses/calculator/function/FunctionFactory.java
@@ -11,6 +11,7 @@ public class FunctionFactory {
 
     private final Map<String, Function> functions =
             new HashMap<String, Function>(){{
+                put("sum", new SumFunction());
             }};
 
     /**
