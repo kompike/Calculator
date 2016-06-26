71386e6f69f541b561b0491d3bc9cd5fc10bd601
kompike <kompanietskostya@gmail.com>
2016-06-26 23:03:21 +0300

    Added test for empty brackets

--- src/test/java/com/javaclasses/calculator/ExceptionTest.java
+++ src/test/java/com/javaclasses/calculator/ExceptionTest.java
@@ -4,7 +4,7 @@ import com.javaclasses.calculator.exception.EvaluationException;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class EvaluationExceptionTest {
+public class ExceptionTest {
 
     private final MathExpressionCalculator calculator =
             new MathExpressionCalculatorImpl();
@@ -19,5 +19,10 @@ public class EvaluationExceptionTest {
             Assert.assertEquals("Caught exception message does not equals expected.",
                     "Can not execute expression after position 0", e.getMessage());
         }
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testEmptyBracketsEvaluation() throws Exception {
+        calculator.evaluate("()");
     }
 }
